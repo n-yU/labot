128f07eada308c2d5f5808351a78618375db3298
nyU <51310314+n-yU@users.noreply.github.com>
2021-03-31 13:50:51 +0900

    Update post/gcalendar.py (add comments)

--- plugins/gcalendar.py
+++ plugins/gcalendar.py
@@ -2,11 +2,12 @@ import requests
 from logging import getLogger, StreamHandler, DEBUG, Formatter
 from datetime import datetime as dt
 from datetime import timedelta
-# from slackbot.bot import listen_to
+from slackbot.bot import listen_to  # デバッグ用
 
 from config import get_config
 import plugins.post as post
 
+# ロガー設定
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
@@ -16,54 +17,71 @@ logger.propagate = False
 handler.setFormatter(Formatter('[labot] %(message)s'))
 
 
-# @listen_to(r'^!gcal$')
+@listen_to(r'^!gcal$')            # デバッグ用
 def main(message):
-    conf = get_config()
-    gas = conf['gcalendar']['gas']
+    """Googleカレンダーから1週間分のイベントを取得し，指定チャンネルにポストする
 
-    start_date = dt.now()
-    # start_date = dt(2021, 2, 8)
-    end_date = start_date + timedelta(days=7)
-    date_params = dict(year=start_date.year, month=start_date.month - 1, day=start_date.day)
+    Args:
+        message ([type]): slackbot.dispatcher.Message
+    """
+    conf = get_config()
+    gas = conf['gcalendar']['gas']          # GAS ウェブアプリURL
+    channel = conf['gcalendar']['channel']  # ポストチャンネル
+
+    # start_date = dt(2021, 2, 8)   # デバッグ用
+    start_date = dt.now()                       # 取得開始日（実行日）
+    end_date = start_date + timedelta(days=7)   # 取得終了日（実行日の7日後）
+    # 取得期間
     period = '{0}/{1}~{2}/{3}'.format(start_date.month, start_date.day, end_date.month, end_date.day)
 
-    # ref.) https://qiita.com/d_kvn/items/5da7f5cdfc8200172a39
+    # ref.) API通信時の例外処理 https://qiita.com/d_kvn/items/5da7f5cdfc8200172a39
     try:
+        # GAS経由でGoogleカレンダーから1週間分のイベント取得
+        date_params = dict(year=start_date.year, month=start_date.month - 1, day=start_date.day)
         response = requests.get(gas, params=date_params)
-        response.raise_for_status()
-        logger.debug('Googleカレンダーから{0}のイベントを取得することに成功しました'.format(period))
+        response.raise_for_status()  # HTTPステータスコード確認
+        logger.debug('Googleカレンダーから{0}のイベント取得に成功しました'.format(period))
     except requests.RequestException as e:
+        # ステータスコード200番台以外 -> エラーログ出力＆メッセージ送信
         logger.debug(e)
-        msg = 'Googleカレンダーから{0}のイベントを取得することに失敗しました（ステータスコード: {1}）．\n \
+        text = 'Googleカレンダーから{0}のイベント取得に失敗しました（ステータスコード: {1}）．\n \
             詳しくはログを確認してください．'.format(period, response.status_code)
 
-        post.slacker_simple_message(channel='bot', msg=msg, _type='error')
+        post.slacker_simple_message(channel=channel, text=text, _type='error')
         return
 
-    weekly_events = response.json()
+    weekly_events = response.json()  # GASから取得したJSONを辞書に変換
+
+    # 曜日インデックスに対する曜日名・カラー
     wod = dict(zip(list(range(7)), ['月', '火', '水', '木', '金', '土', '日']))
     wod_colors = dict(zip(list(range(7)), list(map(lambda x: '#{}'.format(x), conf['gcalendar']['colors']))))
-    attachments = []
 
+    attachments = []    # ref.) attachments https://qiita.com/daikiojm/items/759ea40c00f9b539a4c8
+
+    # 1日ごとに1つのアタッチメントを用意
     for elapsed_days in range(7):
-        date_events = weekly_events.get(str(elapsed_days))
+        date_events = weekly_events.get(str(elapsed_days))  # 取得開始日からelapsed_days日後の日イベント
+        # イベントなし -> アタッチメント作成せず次の日へ
         if date_events is None:
             continue
 
+        # 日付設定
         date = start_date + timedelta(days=elapsed_days)
         text = '*{0:02d}日 ({1})*\n'.format(date.day, wod[date.weekday()])
 
+        # 日イベントの時間とタイトルを順に取得
         for tmp_event_time, event_title in date_events.items():
+            # 00:00~00:00 -> 終日扱い
             event_time = '終日' if tmp_event_time == '00:00~00:00' else tmp_event_time
+            # "イベントタイトル - イベント時間"形式で1イベント1行表示
             text += '{0} - {1}\n'.format(event_time, event_title)
 
-        attc = dict(text=text, color=wod_colors[date.weekday()])
+        attc = dict(text=text, color=wod_colors[date.weekday()])    # アタッチメント（曜日）カラー設定
         attachments.append(attc)
 
-    channel = conf['gcalendar']['channel']
-    msg = '<!channel>\n{}\n\n'.format(conf['gcalendar']['msg'])
-    msg += '*【今週({})のイベント】*\n'.format(period)
-    post.slacker_custom_message(channel=channel, attachments=attachments, text=msg)
+    pre_text = '<!channel>\n{}\n\n'.format(conf['gcalendar']['msg'])
+    pre_text += '*【今週({})のイベント】*\n'.format(period)
+    post.slacker_custom_message(channel=channel, attachments=attachments, pre_text=pre_text)
 
 
 if __name__ == '__main__':
--- plugins/post.py
+++ plugins/post.py
@@ -4,6 +4,7 @@ import json
 from config import get_config, get_slack
 from slackbot.dispatcher import Message
 
+# ロガー設定
 logger = getLogger(__name__)
 handler = StreamHandler()
 handler.setLevel(DEBUG)
@@ -14,38 +15,105 @@ handler.setFormatter(Formatter('[labot] %(message)s'))
 
 
 def get_attachments_by_type(text: str, _type: str) -> List[Dict[str, str]]:
+    """指定メッセージタイプのattachmentsを取得する
+
+    Args:
+        text (str): メッセージテキスト
+        _type (str): メッセージタイプ
+
+    Returns:
+        List[Dict[str, str]]: attachments
+    """
+    # ref.) attachments    https://qiita.com/daikiojm/items/759ea40c00f9b539a4c8
+    # ref.) 配色           http://www.hp-stylelink.com/news/2013/07/20130708.php#lowList01
+
     if _type == 'info':
+        # 情報（正常動作）系
         attachments = [dict(text=text, color='#56a764')]
     else:
+        # エラー系
         if _type != 'error':
-            text = 'エラー'
+            # 未定義のメッセージタイプ指定 -> メッセージタイプを直接ユーザに指定させることはないためコード上のミス
+            text = 'メッセージタイプ `{0}` は未定義です．恐れ入りますが，Twitter `@nyu923` までお知らせください．'
         attachments = [dict(text=text, color='#c93a40')]
 
     return attachments
 
 
 def slackbot_simple_message(message: Message, text: str, _type: str, pre_text='') -> None:
+    """slackbotを使ったシングルアタッチメントメッセージ送信
+
+    Args:
+        message (Message): slackbot.dispatcher.Message
+        text (str): メッセージテキスト
+        _type (str): メッセージタイプ
+        pre_text (str, optional): attachments前テキスト. Defaults to ''.
+    """
     attachments = get_attachments_by_type(text=text, _type=_type)
+
+    # as_user=False -> config.ymlで指定した"name"と"icon"によるtoken発行ユーザによってメッセージ送信
     message.send_webapi('', json.dumps(attachments), as_user=False)
 
 
-def slacker_simple_message(channel: str, msg: str, _type: str) -> None:
-    conf = get_config()
-    slack, uname, icon = get_slack(), conf['name'], conf['icon']
-    attachments = get_attachments_by_type(text=msg, _type=_type)
-    slack.chat.post_message(channel, '', as_user=False, username=uname, icon_url=icon, attachments=attachments)
-
-
-def slacker_custom_message(channel, attachments: List[Dict[str, str]], text='') -> None:
+def slackbot_custom_message(message: Message, attachments: List[Dict[str, str]], text='') -> None:
     pass
 
 
+def slacker_simple_message(channel: str, msg: str, _type: str) -> None:
+    """slackerを使ったシングルアタッチメントメッセージ送信
+
+    Args:
+        channel (str): 送信チャンネル名
+        text (str): メッセージテキスト
+        _type (str): メッセージタイプ
+    """
+    # slackerクライアント＆token発行ユーザ用のユーザ名・アイコン取得
+    conf = get_config()
+    slack, uname, icon = get_slack(), conf['name'], conf['icon']
+
+    attachments = get_attachments_by_type(text=msg, _type=_type)
+
+    # as_user=False -> "username"と"icon_url"によるtoken発行ユーザによってメッセージ送信
+    slack.chat.post_message(channel, '', as_user=False, username=uname, icon_url=icon, attachments=attachments)
+
+
+def slacker_custom_message(channel: str, attachments: List[Dict[str, str]], pre_text='') -> None:
+    """slackerを使ったマルチアタッチメントメッセージ送信
+
+    Args:
+        channel (str): 送信チャンネル名
+        attachments (List[Dict[str, str]]): attachments
+        pre_text (str, optional): attachments前テキスト. Defaults to ''.
+    """
+    conf = get_config()
+    slack, uname, icon = get_slack(), conf['name'], conf['icon']
+    slack.chat.post_message(channel=channel, text=pre_text, attachments=attachments,
+                            as_user=True, username=uname, icon_url=icon)
+
+
 def slacker_simple_ephemeral(message: Message, text: str, _type: str) -> None:
+    """slackerを使ったシングルアタッチメント隠しメッセージ送信
+
+    Args:
+        message (Message): slackbot.dispatcher.Message
+        text (str): メッセージテキスト
+        _type (str): メッセージタイプ
+    """
+    # slackerクライアント＆宛先チャンネル・ユーザID取得
     slack, channel, user = get_slack(), message.body['channel'], message.body['user']
     attachments = get_attachments_by_type(text=text, _type=_type)
+
+    # 隠しメッセージ - "only visible to you"と表示される自分自身のみに表示されるメッセージ (ephemeral)
     slack.chat.post_ephemeral(channel=channel, text='', attachments=attachments, user=user, as_user=True)
 
 
-def slacker_custom_ephemeral(message: Message, attachments: List[Dict[str, str]], text='') -> None:
+def slacker_custom_ephemeral(message: Message, attachments: List[Dict[str, str]], pre_text='') -> None:
+    """slackerを使ったマルチアタッチメント隠しメッセージ送信
+
+    Args:
+        message (Message): slackbot.dispatcher.Message
+        attachments (List[Dict[str, str]]): attachments
+        pre_text (str, optional): attachments前テキスト. Defaults to ''.
+    """
     slack, channel, user = get_slack(), message.body['channel'], message.body['user']
-    slack.chat.post_ephemeral(channel=channel, text=text, attachments=attachments, user=user, as_user=True)
+    slack.chat.post_ephemeral(channel=channel, text=pre_text, attachments=attachments, user=user, as_user=True)
